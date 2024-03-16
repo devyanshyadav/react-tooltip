622e744316fef90e6ded0ddf5d3a124262c94548
gabrieljablonski <contact@gabrieljablonski.com>
2024-03-15 21:42:57 -0300

    chore: remove deprecated `anchorById` and refactor `anchorsBySelect`

--- src/components/Tooltip/TooltipTypes.d.ts
+++ src/components/Tooltip/TooltipTypes.d.ts
@@ -115,11 +115,6 @@ export interface ITooltip {
   offset?: number
   id?: string
   variant?: VariantType
-  /**
-   * @deprecated Use the `data-tooltip-id` attribute, or the `anchorSelect` prop instead.
-   * See https://react-tooltip.com/docs/getting-started
-   */
-  anchorId?: string
   anchorSelect?: string
   wrapper: WrapperType
   /**
